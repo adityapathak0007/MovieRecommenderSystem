ae6d6353536295c40f1bf8b21defc8424df08bc0
Aditya Pathak <adityapathak034@gmail.com>
2024-09-14 15:08:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ import pandas as pd
 import requests
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    return f"https://image.tmdb.org/t/p/w500/{poster_path}"
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -21,48 +19,36 @@ def recommend(movie):
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
         recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies,recommended_movies_posters
-
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
+    return recommended_movies, recommended_movies_posters
 
 def download_file(url, filename):
     response = requests.get(url)
     with open(filename, 'wb') as file:
         file.write(response.content)
 
-file_url = "https://drive.google.com/file/d/1nRxFIkLs-lfRtUVozJCUAEssNkiigzd8/view?usp=sharing"
+# Use direct download link
+file_url = "https://drive.google.com/uc?id=1nRxFIkLs-lfRtUVozJCUAEssNkiigzd8"
 download_file(file_url, 'similarity.pkl')
 
 # Now load the file
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+with open('similarity.pkl', 'rb') as file:
+    similarity = pickle.load(file)
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
 st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox(
-    'How would you like to be connected',
-movies['title'].values
+    'Select a movie',
+    movies['title'].values
 )
 
 if st.button('Show Recommendation'):
-    recommended_movies,recommended_movies_posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movies[0])
-        st.image(recommended_movies_posters[0])
-    with col2:
-        st.text(recommended_movies[1])
-        st.image(recommended_movies_posters[1])
-
-    with col3:
-        st.text(recommended_movies[2])
-        st.image(recommended_movies_posters[2])
-    with col4:
-        st.text(recommended_movies[3])
-        st.image(recommended_movies_posters[3])
-    with col5:
-        st.text(recommended_movies[4])
-        st.image(recommended_movies_posters[4])
-
+    recommended_movies, recommended_movies_posters = recommend(selected_movie_name)
+    cols = st.columns(5)
+    for col, movie, poster in zip(cols, recommended_movies, recommended_movies_posters):
+        with col:
+            st.text(movie)
+            st.image(poster)
